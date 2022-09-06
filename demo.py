9817a0fe37abbb590aff3859c70d37750d27db9b
Bernardo Janko Goncalves Biesseck <bjgbiesseck@web.ufpr.br>
2022-09-06 16:32:06 -0300

    Adjusts 'demo.py': add Tree class to read all folders into 'demo/input'

--- demo.py
+++ demo.py
@@ -48,6 +48,8 @@ def deterministic(rank):
     cudnn.benchmark = False
 
 
+'''
+# ORIGINAL
 def process(args, app, image_size=224):
     dst = Path(args.a)
     dst.mkdir(parents=True, exist_ok=True)
@@ -73,6 +75,38 @@ def process(args, app, image_size=224):
         processes.append(file + '.npy')
 
     return processes
+'''
+
+
+# BERNARDO
+def process_BERNARDO(args, app, image_size=224):
+    dst = Path(args.a)
+    dst.mkdir(parents=True, exist_ok=True)
+    processes = []
+    # image_paths = sorted(glob(args.i + '/*.*'))   # original
+    image_paths = sorted(glob(args.i + '/*.jpg'))   # BERNARDO
+    for image_path in tqdm(image_paths):
+        name = Path(image_path).stem
+        img = cv2.imread(image_path)
+        # print('demo.py: process_BERNARDO(): image_path=', image_path)
+        bboxes, kpss = app.det_model.detect(img, max_num=0, metric='default')
+        if bboxes.shape[0] == 0:
+            continue
+        i = get_center(bboxes, img)
+        bbox = bboxes[i, 0:4]
+        det_score = bboxes[i, 4]
+        kps = None
+        if kpss is not None:
+            kps = kpss[i]
+        face = Face(bbox=bbox, kps=kps, det_score=det_score)
+        blob, aimg = get_arcface_input(face, img)
+        file = str(Path(dst, name))
+        np.save(file, blob)
+        cv2.imwrite(file + '.jpg', face_align.norm_crop(img, landmark=face.kps, image_size=image_size))
+        processes.append(file + '.npy')
+
+    return processes
+
 
 
 def to_batch(path):
@@ -99,8 +133,32 @@ def load_checkpoint(args, mica):
         mica.flameModel.load_state_dict(checkpoint['flameModel'])
 
 
+# BERNARDO
+class Tree:
+    def __init__(self):
+        # prefix components:
+        self.space = '    '
+        self.branch = '│   '
+        # pointers:
+        self.tee = '├── '
+        self.last = '└── '
+
+    def walk(self, dir_path: Path, output_path=''):
+        contents = list(dir_path.iterdir())
+        pointers = [self.tee] * (len(contents) - 1) + [self.last]
+        for pointer, path in zip(pointers, contents):
+            if path.is_dir():  # extend the prefix and recurse:
+                yield str(path)
+                output_path_dir = output_path + '/' + path.name
+                # Path(output_path_dir).mkdir(parents=True, exist_ok=True)
+                yield from self.walk(path, output_path_dir)
+
+
+
 def main(cfg, args):
-    device = 'cuda:0'
+    device = 'cuda:0'  # original
+    # device = 'cuda:1'    # BERNARDO
+
     cfg.model.testing = True
     mica = util.find_model_using_name(model_dir='micalib.models', model_name=cfg.model.name)(cfg, device)
     load_checkpoint(args, mica)
@@ -114,38 +172,51 @@ def main(cfg, args):
 
     with torch.no_grad():
         logger.info(f'Processing has started...')
-        paths = process(args, app)
-        for path in tqdm(paths):
-            name = Path(path).stem
-            images, arcface = to_batch(path)
-            codedict = mica.encode(images, arcface)
-            opdict = mica.decode(codedict)
-            meshes = opdict['pred_canonical_shape_vertices']
-            code = opdict['pred_shape_code']
-            lmk = mica.flame.compute_landmarks(meshes)
 
-            mesh = meshes[0]
-            landmark_51 = lmk[0, 17:]
-            landmark_7 = landmark_51[[19, 22, 25, 28, 16, 31, 37]]
-            rendering = mica.render.render_mesh(mesh[None])
-            image = (rendering[0].cpu().numpy().transpose(1, 2, 0).copy() * 255)[:, :, [2, 1, 0]]
-            image = np.minimum(np.maximum(image, 0), 255).astype(np.uint8)
+        # LIST ALL DIRS (BERNARDO)
+        subfolders = [args.i]
+        for subfolder in Tree().walk(Path('.') / args.i, args.o):
+            subfolders.append(subfolder)
 
-            dst = Path(args.o, name)
-            dst.mkdir(parents=True, exist_ok=True)
-            cv2.imwrite(f'{dst}/render.jpg', image)
-            save_ply(f'{dst}/mesh.ply', verts=mesh.cpu() * 1000.0, faces=faces)  # save in millimeters
-            save_obj(f'{dst}/mesh.obj', verts=mesh.cpu() * 1000.0, faces=faces)
-            np.save(f'{dst}/identity', code[0].cpu().numpy())
-            np.save(f'{dst}/kpt7', landmark_7.cpu().numpy() * 1000.0)
-            np.save(f'{dst}/kpt68', lmk.cpu().numpy() * 1000.0)
+        for args.i in subfolders:
+            args.a = args.i.replace('input', 'arcface')
+            args.o = args.i.replace('input', 'output')
 
-        logger.info(f'Processing finished. Results has been saved in {args.o}')
+            # paths = process(args, app)   # original
+            paths = process_BERNARDO(args, app)   # BERNARDO
+
+            for path in tqdm(paths):
+                name = Path(path).stem
+                images, arcface = to_batch(path)
+                codedict = mica.encode(images, arcface)
+                opdict = mica.decode(codedict)
+                meshes = opdict['pred_canonical_shape_vertices']
+                code = opdict['pred_shape_code']
+                lmk = mica.flame.compute_landmarks(meshes)
+
+                mesh = meshes[0]
+                landmark_51 = lmk[0, 17:]
+                landmark_7 = landmark_51[[19, 22, 25, 28, 16, 31, 37]]
+                rendering = mica.render.render_mesh(mesh[None])
+                image = (rendering[0].cpu().numpy().transpose(1, 2, 0).copy() * 255)[:, :, [2, 1, 0]]
+                image = np.minimum(np.maximum(image, 0), 255).astype(np.uint8)
+
+                dst = Path(args.o, name)
+                dst.mkdir(parents=True, exist_ok=True)
+                cv2.imwrite(f'{dst}/render.jpg', image)
+                save_ply(f'{dst}/mesh.ply', verts=mesh.cpu() * 1000.0, faces=faces)  # save in millimeters
+                save_obj(f'{dst}/mesh.obj', verts=mesh.cpu() * 1000.0, faces=faces)
+                np.save(f'{dst}/identity', code[0].cpu().numpy())
+                np.save(f'{dst}/kpt7', landmark_7.cpu().numpy() * 1000.0)
+                np.save(f'{dst}/kpt68', lmk.cpu().numpy() * 1000.0)
+
+            logger.info(f'Processing finished. Results has been saved in {args.o}')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='MICA - Towards Metrical Reconstruction of Human Faces')
-    parser.add_argument('-i', default='demo/input', type=str, help='Input folder with images')
+    # parser.add_argument('-i', default='demo/input', type=str, help='Input folder with images')  # original
+    parser.add_argument('-i', default='demo/input_TESTE', type=str, help='Input folder with images')    # BERNARDO
     parser.add_argument('-o', default='demo/output', type=str, help='Output folder')
     parser.add_argument('-a', default='demo/arcface', type=str, help='Processed images for MICA input')
     parser.add_argument('-m', default='data/pretrained/mica.tar', type=str, help='Pretrained model path')
