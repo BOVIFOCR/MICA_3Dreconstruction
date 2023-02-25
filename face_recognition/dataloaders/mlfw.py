376c9d5da01e16325929daa9ba9eee7f9aaf3e23
Bernardo Biesseck <biesseck@gmail.com>
2023-02-25 19:21:12 -0300

    face_recognition/dataloaders/lfw.py: remove extra whitespaces

--- face_recognition/dataloaders/mlfw.py
+++ face_recognition/dataloaders/mlfw.py
@@ -18,7 +18,7 @@ class MLFW_3DMM(torch.utils.data.Dataset):
         self.classes = sorted(list(set([self._class_name_from_path_sample(path) for path in self.paths])))
         self.num_classes = len(self.classes)
         self.class_to_idx = {cls_name: i for i, cls_name in enumerate(self.classes)}
-    
+
     def _class_name_from_path_sample(self, path=''):
         class_name = '_'.join(path.split('/')[-2].split('_')[:-3])
         return class_name
@@ -35,7 +35,7 @@ class MLFW_3DMM(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.paths)
-    
+
     def __getitem__(self, index: int):
         sample_data = torch.from_numpy(self.load_sample(index)).to(self.device)
         # class_name  = self.paths[index].parent.name                         # expects path in data_folder/class_name/image.jpeg
